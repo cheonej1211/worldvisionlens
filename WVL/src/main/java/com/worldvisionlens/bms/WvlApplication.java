9dd85cc4157d753b68583cee7cd5e61a2c638be7
cheonej1211 <cheonej95@naver.com>
2023-07-24 15:07:10 +0900

    [aws & rds 설정완료]

---
+++ WVL/src/main/java/com/worldvisionlens/bms/WvlApplication.java
@@ -0,0 +1,13 @@
+package com.worldvisionlens.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WvlApplication.class, args);
+	}
+
+}
---
+++ WVL/src/test/java/com/worldvisionlens/bms/WvlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldvisionlens.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WvlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
